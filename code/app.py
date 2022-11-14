24c2e75df9285f450453e6bc0c73236fde0be51e
Satish Raju <satishbraju@gmail.com>
2022-11-14 16:48:38 +0100

    adding code

---
+++ code/app.py
@@ -0,0 +1,8 @@
+import re
+
+str = 'satish'
+
+print('hello', str)
+print('check with path')
+#check if the changes were ok
+#check post merge changes
---
+++ code/slp.py
@@ -0,0 +1,7 @@
+import time
+
+secnds = 30
+
+time.sleep(secnds)
+print('i slept for,' ,secnds, 'seconds')
+print('i slept for,' ,secnds, 'seconds')
