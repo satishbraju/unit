25e242ec9733321993d8f5fb4e6e39c4052e5dda
Satish Raju <satishbraju@gmail.com>
2022-11-15 11:33:38 +0100

    adding new feature

---
+++ code/NewApp.py
@@ -0,0 +1,14 @@
+import re
+
+str = 'satish'
+
+print('hello', str)
+print('check with path')
+#check if the changes were ok
+#check post merge changes
+#changes in submodule
+#changes in submodule
+#dispatcher check
+#dispatcher check
+#dispatcher check works
+#id check
